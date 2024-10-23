f88be8607759a25445e9c686dc3b5a57c690866f
117CarlosCoder <carloslopez202031871@cunoc.edu.gt>
2024-10-23 12:40:40 -0600

    Agregando carpetas con cabios para windows

---
+++ Scripts/zoom_in.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Haciendo zoom in...
+REM Simula la pulsación de teclas para hacer zoom in
+"C:\Users\Carlos\Documents\ArduinoPython\ArduinoPython\Scripts\nircmd.exe" sendkeypress ctrl + +
---
+++ Scripts/zoom_out.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Haciendo zoom out...
+REM Simula la pulsación de teclas para hacer zoom out
+"C:\Users\Carlos\Documents\ArduinoPython\ArduinoPython\Scripts\nircmd.exe" sendkeypress ctrl + -
